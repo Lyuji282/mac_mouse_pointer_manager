26cca35d2272b66c02cdcdc310dd00733a0cfdd2
Arthur Lawrence <arthurlawrence@js-MacBook-Pro.local>
2019-11-18 16:57:21 +0900

    Add second monitor center

--- build.sh
+++ build.sh
@@ -4,3 +4,6 @@ gcc -o build/upper_right.out upper_right.c -Wall -framework ApplicationServices
 
 gcc -o build/lower_right.out lower_right.c -Wall -framework ApplicationServices
 gcc -o build/lower_left.out lower_left.c -Wall -framework ApplicationServices
+
+
+gcc -o build/upper_center_2.out upper_center_2.c -Wall -framework ApplicationServices
